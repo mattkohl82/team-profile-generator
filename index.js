22449997c015f0b74eb2736a8e28f5d6de520f55
mattkohl82 <mattkohl82@gmail.com>
2020-05-23 22:36:52 -0500

    html generating and css styled

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 //const generateHTML = require('./src/generateHtml')
 
-
+//Array for employees
 const employees = []
 
 //Manager questions
 const managerQuestions = () => {
-    console.log(`
-=================
-Create a Team Profolio!!
-=================
-    `)
     return inquirer.prompt([
         {
             type: 'input',
@@ -72,7 +67,7 @@ Create a Team Profolio!!
         }, 
     ])
     .then(data => {
-    const manager = new Manager(data.name, data.id, data.email, data.officeNumber)
+    const manager = new Manager(data.name, data.id, data.email, data.number, data.icon)
     employees.push(manager)
     typeQuestion()
     })
@@ -84,7 +79,7 @@ const engQuestions = () => {
         {
             type: 'input',
             name: 'name',
-            message : 'What is the Enginners name?',
+            message : "What is the Engineer's name?",
             validate: nameInput => {
                 if (nameInput) {
                   return true;
@@ -97,7 +92,7 @@ const engQuestions = () => {
         {
             type: 'input',
             name: 'email',
-            message: 'What is Engineers email adress?',
+            message: "What is Engineer's email adress?",
             validate: emailInput => {
                 if (emailInput) {
                   return true;
@@ -111,7 +106,7 @@ const engQuestions = () => {
         {
             type: 'input',
             name: 'id',
-            message: 'What is the Project Managers Employee ID?',
+            message: "What is the Engineer's Employee ID?",
             validate: idInput => {
                 if (idInput) {
                   return true;
@@ -124,7 +119,7 @@ const engQuestions = () => {
         {
             type: 'input',
             name: 'github',
-            message: 'What is the Engineers Github?',
+            message: "What is the Engineer's Github?",
             validate: idInput => {
                 if (idInput) {
                   return true;
@@ -136,7 +131,7 @@ const engQuestions = () => {
         }
     ])
     .then(data => {
-        const engineer = new Engineer(data.name, data.id, data.email, data.github)
+        const engineer = new Engineer(data.name, data.id, data.email, data.github, data.icon)
         employees.push(engineer)
         typeQuestion()
     })
@@ -200,7 +195,7 @@ const intQuestion = () => {
         }
     ])
     .then(data => {
-        const intern = new Intern(data.name, data.id, data.email, data.school)
+        const intern = new Intern(data.name, data.id, data.email, data.school, data.icon)
         employees.push(intern)
         typeQuestion()
     })
@@ -216,51 +211,99 @@ const typeQuestion = () => {
             choices: [
                 'Engineer',
                 'Intern',
-                'no more'
+                'No more employees to add'
                 ]
         }
       ]).then(data => {
         switch(data.addToTeam) {
             case 'Engineer':
-                engQuestions();
+                engQuestions()
                 break;
             case 'Intern':
-                intQuestion();
+                intQuestion()
                 break;
-            case 'I don\'t want to add any other members':
-                generateHTML(employees);
-            //default: 
-                //generateHTML);
+            case 'No more employees to add':
+                generateHTML()
+                break;
         }
     });
 }
 
-const generateHTML = (employees) => {
-  for (let i = 0; i < employees.length; i++) {
-      return `
-      <!DOCTYPE html> 
+function generateHTML ()  {
+  const html = [];
+  const startHtml =`
+<!DOCTYPE html> 
 <html lang="en"> 
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Team</title>
+<title>My Team</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
-      <h1> ${ this.name } </h1>
-      <h1> ${ this.title } </h1>
-      <h1> ${ this.id } </h1>
-      <h1> ${ this.school } </h1>
-      <h1> ${ this.github } </h1>
-      <h1> ${ this.number } </h1>
-      <h1> ${ this.email } </h1>
-  </body>
+<header class="bg-success">
+  <div class="container flex-row justify-space-between align-center py-3">
+      <h1 class="page-title text-dark text-center">The Team</h1>
+  </div>
+</header>
+  <div class="container mt-5">
+  <div class="row mx-auto d-flex justify-content-center">
+`
+html.push(startHtml);
+  for (let i = 0; i < employees.length; i++) {
+      let person = `
+      <div class="col-sm-3 mb-5">
+      <div class="card bg-light" style="max-width: 18rem; min-width: 14rem;">
+      <div class="card-header text-dark text-center bg-success mb-3">
+          <h2>${ employees[i].name }</h2>
+          <h3>${ employees[i].icon }${ employees[i].title }</h3>
+      </div>
+      <div>
+          <p class="card-text m-3 border border-dark text-center"><i class="far fa-id-badge mr-2"></i>Employee ID: ${ employees[i].id }</p>
+          <p class="card-text m-3 border border-dark text-center"><i class="fas fa-at mr-2"></i>Email: <a href= "mailto:${employees[i].email}"> ${ employees[i].email } </a></p>
       `
-    }
-  fs.writeFile('./dist/index.html', generateHTML(data), err => {
-          if (err) throw err;
-          console.log('Team Profile created! Check out index.html in dist directory to see it!')
-  })
+      
+      if (employees[i].number){
+          person += `
+          <p class="card-text m-3 border border-dark text-center"><i class="fas fa-door-open mr-2"></i>Office number: ${ employees[i].number }</p>
+          `
+      }
+      if (employees[i].github){
+          person += `
+          <p class="card-text m-3 border border-dark text-center"><i class="fab fa-github mr-2"></i>GitHub: <a href="https://github.com/${ employees[i].github }" target="_blank">${ employees[i].github }</a></p>
+          `
+      }
+      if (employees[i].school){
+          person += `
+          <p class="card-text m-3 border border-dark text-center"><i class="fas fa-graduation-cap mr-2"></i>School: ${ employees[i].school }</p>
+          `
+      }
+      
+      person +=
+      `
+      </div>
+      </div>
+      </div>
+      `;
+      html.push(person)
+  }
+  
+const endHTML = `
+      </div>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  </body>
+</html>
+`
+html.push(endHTML);
+
+fs.writeFile('./dist/index.html', html.join(""), err => {
+  if (err) throw err;
+  console.log('Team Profile created! Check out index.html in dist directory to see it!')
+});
 }
 
 // function call to initialize program
